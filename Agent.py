48f72ae4c6bb9d4df7b6a8b83f139634ed015bfa
vivokas20 <vivianabernardo@tecnico.ulisboa.pt>
2020-05-25 17:41:37 +0100

    correct print identation

--- Agent.py
+++ Agent.py
@@ -181,7 +181,7 @@ class Agent:
                         print("Agent", self.agent_id, "-All zone hospitals are incapable of allocating all patients to attend.\n"
                                                       "Will contact nearest zone(s) to ask for help")
                         with open('plots/hospitals/out1_2.txt', 'a') as f:
-                          print('+ 1', file=f)
+                            print('+ 1', file=f)
                     agent_help_hospital = self.help_hospital(emergency, self.other_agents)
                     if agent_help_hospital is None:
                         print("Agent", self.agent_id, "-Error: All hospitals are full or don't have capacity for number of pacientes, emergency failed!")
